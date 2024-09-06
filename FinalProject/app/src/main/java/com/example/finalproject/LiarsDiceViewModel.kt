0db618aefefad6c9c69c11c86e68cfe396fc6644
Dylan-B-Johnson <dmaxbear123@gmail.com>
2024-09-05 22:18:27 -0400

    Didn't recognize project, so un-changed the name

--- FinalProject/app/src/main/java/com/example/finalproject/LiarsDiceViewModel.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/LiarsDiceViewModel.kt
@@ -8,7 +8,7 @@ class LiarsDiceViewModel() : ViewModel() {
         1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
     )
 
-    var game = LiarsDiceGame(strategies = listOf(Conservative, Random))
+    var game = LiarsDiceGame(strategies = listOf(Conservative))
 
     fun getAllowedBets() : Array<Int> {
         return allowedBets
